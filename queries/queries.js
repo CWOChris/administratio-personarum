811c71e57bb6acccca87d80e92d3a7c8daec5b6a
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-22 19:02:40 -0600

    error troubleshooting in queries:

--- queries/queries.js
+++ queries/queries.js
@@ -22,11 +22,19 @@ const queries = {
     )`,
     createRolesTable: `CREATE TABLE roles (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL,
+        title VARCHAR(30) NOT NULL,
+        salary DECIMAL(10,2) NOT NULL,
+        department_id INT,
+        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
     )`,
     createEmployeeTable: `CREATE TABLE employee (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT,
+        manager_id INT,
+        FOREIGN KEY (role_id) REFERENCES roles(id),
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
     )`,
 };
 
