74bca004455f6629c285234d679d2cbec7dac883
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-24 18:11:31 -0600

    finished the employee.js

--- db/db.js
+++ db/db.js
@@ -1,11 +1,10 @@
-const { Sequelize } = require('sequelize');
+const mysql = require('mysql2');
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
+const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'employee_tracker'
 });
 
-module.exports = sequelize;
+module.exports = pool.promise();
--- models/employee.js
+++ models/employee.js
@@ -1,34 +1,26 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db/db');
+const mysql = require('mysql2');
+const pool = require('../db/db.js');
 
-const Employee = sequelize.define('Employee', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    role_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    manager_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-}, {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'employee',
-});
+const createEmployeeTableQ = `
+    CREATE TABLE IF NOT EXISTS employee (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        role_id INT NOT NULL,
+        manager_id INT,
+        FOREIGN KEY (role_id) REFERENCES role(id),
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
+    )`;
 
-module.exports = Employee;
+    pool.query(createEmployeeTableQ, (err, results) => {
+        if (err) {
+            console.error('Problem creating employee', err);
+        } else {
+            console.log('Employee created successfully');
+        }
+    });
+
+    function addEmployee(employeeData, callback) {
+        const { first_name, last_name, role_id, manager_id } = employeeData;
+        const addEmployeeQ = 'INSERT INTO employee'
+    }
